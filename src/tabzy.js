b9456cd742873691ab9a35776f97d115e641ebda
huynhtrong007 <huynhtrongz@gmail.com>
2025-05-29 14:07:40 +0700

    Xây dựng logic khởi tạo cho tabzy

---
+++ src/tabzy.js
@@ -0,0 +1,48 @@
+function Tabzy(selector){
+    this.container = document.querySelector(selector);
+    if (!this.container) {
+        console.error(`Tabzy: No container found for selector '${selector}'`);
+    }
+    
+    this.tabs = Array.from(this.container.querySelectorAll("li a"));
+    if (!this.tabs.length) {
+        console.log('Tabzy: No tabs found inside the container');
+        return;
+    }
+
+    this.panels = this.tabs.map((tab) => {
+        const panel = document.querySelector(tab.getAttribute("href"));
+        if (!panel) {
+            console.error(
+                `Tabzy: No panel found for selector '${tab.getAttribute("href")}'`
+            );
+        }
+        return panel;
+    }).filter(Boolean)
+
+    if (this.tabs.length !== this.panels.length) return;
+    this._init();
+} 
+
+Tabzy.prototype._init = function() {
+    const tabActive = this.tabs[0];
+    tabActive.closest("li").classList.add("tabzy--active");
+    this.panels.forEach((panel) => (panel.hidden = true));
+    this.tabs.forEach((tab) => {
+            tab.onclick = (event) => this._handleTabClick(event,tab);
+    })
+    const panelActive = this.panels[0];
+    panelActive.hidden = false;
+    
+}
+
+Tabzy.prototype._handleTabClick = function(event,tab) {
+    event.preventDefault();
+    this.tabs.forEach((tab) => {
+        tab.closest('li').classList.remove('tabzy--active')
+    })
+    tab.closest("li").classList.add("tabzy--active");
+    this.panels.forEach((panel) => (panel.hidden = true));
+    const panelActive = document.querySelector(tab.getAttribute("href"));
+    panelActive.hidden = false;   
+}
